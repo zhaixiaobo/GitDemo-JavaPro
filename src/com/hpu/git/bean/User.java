108a56cda420a61e57a268e37eb6669125ed77de
zhaixiaobo <zhaixiaobo12@163.com>
2017-05-31 22:54:45 +0800

    提交文件

---
+++ src/com/hpu/git/bean/User.java
@@ -0,0 +1,16 @@
+package com.hpu.git.bean;
+/** 
+ * @author 作者 :huahua 
+ * @date 创建时间：2017年5月31日 下午10:52:59 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return
+ */
+public class User {
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
