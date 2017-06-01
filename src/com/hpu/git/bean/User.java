358d6227b79fb58af90dbfdf5065265cae0fbc59
zhaixiaobo <zhaixiaobo12@163.com>
2017-06-01 13:41:59 +0800

    bean中User类修改

--- src/com/hpu/git/bean/User.java
+++ src/com/hpu/git/bean/User.java
@@ -9,10 +9,34 @@ package com.hpu.git.bean;
  */
 public class User {
 
+	private Integer id;
 	private String userName;
 	private String password;
 	public User() {
 		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + "]";
 	}
 	
 }
