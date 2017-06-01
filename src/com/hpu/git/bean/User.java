39055349fe295a8bf2ed85a086b61b3f55fab9b8
zhaixiaobo <zhaixiaobo12@163.com>
2017-06-01 14:19:58 +0800

    bean中User类加了age属性

--- src/com/hpu/git/bean/User.java
+++ src/com/hpu/git/bean/User.java
@@ -12,6 +12,8 @@ public class User {
 	private Integer id;
 	private String userName;
 	private String password;
+	private int age;
+	
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
@@ -21,17 +23,26 @@ public class User {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getUserName() {
 		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 	
 	@Override
